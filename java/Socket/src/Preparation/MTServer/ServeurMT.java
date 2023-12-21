a663ea5535b82cc5d4e2fb20f78bcef3c1636152
hamzajaada <hamzajaada22@gmail.com>
2023-12-21 14:56:42 +0100

    push

--- java/Socket/src/Preparation/MTServer/ServeurMT.java
+++ java/Socket/src/Preparation/MTServer/ServeurMT.java
@@ -7,14 +7,17 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class ServeurMT extends Thread {
     private boolean isActive = true;
     private int nombreClient = 0 ;
-    private int nombreSecret ;
-    private boolean fin;
-    private String gagnant;
+    // private int nombreSecret ;
+    // private boolean fin;
+    private List<Conversation> Clients = new ArrayList<Conversation>();
+    // private String gagnant;
     public static void main(String[] args) {
         // pour demarrer le Threads pour executer la methode run de Server socket:        
         new ServeurMT().start();
@@ -26,7 +29,7 @@ public class ServeurMT extends Thread {
             //creation de server :
              ServerSocket serverSocket = new ServerSocket(1234);
              System.out.println("runnnig !!!");
-             nombreSecret = 6;
+            //  nombreSecret = 6;
              // toujour Active :
              while (isActive) {
                 //attend un client se connecter et return se socket:
@@ -34,7 +37,11 @@ public class ServeurMT extends Thread {
                 //  dans chaque connection le nombre augmenter
                 ++nombreClient;
                 // creer un nouvelle conversation et appele la methode run (thread de socket )
-                new Conversation(socket,nombreClient).start();
+
+              Conversation con =new Conversation(socket,nombreClient);
+              Clients.add(con);
+              con.start();
+
              }
         } catch (Exception e) {
             e.printStackTrace();
@@ -43,11 +50,29 @@ public class ServeurMT extends Thread {
     }
 }
     class Conversation extends Thread {
-        private Socket socketClient;
-        private int num;
+        protected Socket socketClient;
+        protected int num;
         public Conversation(Socket socketClient , int n){
             this.socketClient = socketClient;
             this.num= n;
+        }
+        
+        public void EnvoyerMsgToAllClient(String msg,Socket soc , int num){
+            try {
+                for(Conversation Client : Clients){
+                    if(Client.socketClient!=soc){
+                        if (Client.num == num || num == -1) {
+                             PrintWriter pw = new PrintWriter(Client.socketClient.getOutputStream(),true);
+                             pw.println("envoyer"+msg);
+                        }
+                       
+                    }
+                    
+            }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
         }
         @Override
         public void run(){
@@ -61,32 +86,53 @@ public class ServeurMT extends Thread {
                 String ipClient = socketClient.getRemoteSocketAddress().toString();
                 pw.println("bienvenue vous etes les client numero ="+num);
                 System.out.println("connexion de client numero "+num+" IP = "+ipClient);
-                pw.println("deviner le nombre secret");
+                // System.out.println(Clients);
+                // pw.println("deviner le nombre secret");
                 while (true) {
-                    //lire la req envoyer par le client 
-                    String req = br.readLine();
-                    int nombre = Integer.parseInt(req);
-                    //calculer le lenght
-                    // String reponse = "lenght ="+req.length();
-                    // // envoyer la reponse
-                    // pw.println(reponse);
-                    if(fin == false){
-                            if(nombre>nombreSecret){
-                                pw.println("votre nombre est superieur au nombre secret");
+                        String req = br.readLine();
+                        
+                        if(req.contains(">")){
+                            String [] request = req.split(">");
+                            if (request.length==2) {
+                                String message = request[1];
+                                int nume = Integer.parseInt(request[0]);
+                                EnvoyerMsgToAllClient(message,socketClient,nume);
+                                
                             }
-                            else if(nombre<nombreSecret){
-                                pw.println("votre nombre est inferieur au nombre secret");
-                            }
-                            else{
-                                pw.println("Bravo vous avez gagner");
-                                gagnant = ipClient;
-                                System.out.println("bravo eux gagant IP = "+gagnant);
-                                fin=true;
-                            }
-                    }
-                    else{
-                        pw.println("Jeu terminer le gagant est = "+gagnant);
-                    }
+
+                        }
+                        else{
+                        EnvoyerMsgToAllClient(req,socketClient,-1);
+
+                        }
+                        
+
+
+
+                    // //lire la req envoyer par le client 
+                    // String req = br.readLine();
+                    // int nombre = Integer.parseInt(req);
+                    // //calculer le lenght
+                    // // String reponse = "lenght ="+req.length();
+                    // // // envoyer la reponse
+                    // // pw.println(reponse);
+                    // if(fin == false){
+                    //         if(nombre>nombreSecret){
+                    //             pw.println("votre nombre est superieur au nombre secret");
+                    //         }
+                    //         else if(nombre<nombreSecret){
+                    //             pw.println("votre nombre est inferieur au nombre secret");
+                    //         }
+                    //         else{
+                    //             pw.println("Bravo vous avez gagner");
+                    //             gagnant = ipClient;
+                    //             System.out.println("bravo eux gagant IP = "+gagnant);
+                    //             fin=true;
+                    //         }
+                    // }
+                    // else{
+                    //     pw.println("Jeu terminer le gagant est = "+gagnant);
+                    // }
                 }
             } catch (IOException e) {
                e.printStackTrace(); 
